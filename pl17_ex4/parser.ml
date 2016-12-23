36a0c334e036f661e71acdf359d511cae7ca9d63
dhjkd <dhjkd>
2016-12-23 12:21:47 +0200

    add q1 - before test

--- pl17_ex4/parser.ml
+++ pl17_ex4/parser.ml
@@ -24,3 +24,40 @@ exception SyntaxError of string
 (*
   ADD FUNCTIONS BELOW
 *)
+
+let rec parse_term ts = 
+	match ts with
+  | (Literal id) :: ts'->(Variable id) ,ts' 
+  | LParen :: LambdaTok :: (Literal id) :: DotTok :: ts'-> let (r,ts'')=parse_term ts' in (match ts'' with
+  																		| RParen::ts'' -> Abstraction (id,r), ts''
+  																		| _ -> raise (SyntaxError "Invalid syntax: LParen is expected.\n")
+  																	)
+  | LParen :: ts' -> let (r,ts'')=parse_term ts' in (match ts'' with
+  										| RParen::ts''' -> r,ts'''
+  										| [] -> raise (SyntaxError "Invalid syntax.\n")
+  										| _ -> let (r',ts''')=parse_term ts'' in (match ts''' with
+  																			| RParen::ts'''' -> Application(r,r'),ts''''
+  																			| _ -> raise (SyntaxError "Invalid syntax: RParen is expected.\n")
+  																		    )
+  										)
+  | LetTok :: (Literal id) :: EqTok :: ts' -> let (r,ts'')=parse_term ts' in (match ts'' with
+  															| InTok :: ts''' -> let (r',ts'''') = parse_term ts''' in (match ts'''' with	
+  																												| _ -> Application(Abstraction(id,r'),r),ts''''
+  																											    )
+  															| _ -> raise (SyntaxError "Invalid syntax: InTok is expected.\n")
+  																						)
+  | _ -> raise (SyntaxError "Invalid syntax of term\n")
+
+
+let parse s = let (r, ts) =
+		 s |> string_to_list |> tokenize |> parse_term in
+	      match ts with
+	      | [] -> r
+	      | _ -> raise (SyntaxError "Unexpected input.\n")
+
+let rec format_term = function
+  | Variable str -> str
+  | Abstraction (str,t) -> "("^"\\"^str^"."^(format_term t)^")"
+  | Application (t1,t2) -> let t1'=format_term t1 in
+  						 let t2'=format_term t2 in
+  						 "("^t1'^" "^t2'^")"
