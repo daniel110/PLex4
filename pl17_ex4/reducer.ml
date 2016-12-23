90c62c440e3c182aed09558c452eae827b4f42cb
dhjkd <dhjkd>
2016-12-23 16:53:29 +0200

    add qs2 before test

--- pl17_ex4/reducer.ml
+++ pl17_ex4/reducer.ml
@@ -12,6 +12,13 @@ exception OutOfVariablesError
 let possible_variables = List.map (fun x -> char_to_string (char_of_int x)) ((range 97 123) @ (range 65 91))
 
 
+(* I used StringSet operations from here: https://ocaml.org/learn/tutorials/modules.html *)
+let rec fv term = function
+| Variable x -> StringSet.singelton x
+| Abstraction x,t -> StringSet.remove x (fv t)
+| Application t1,t2 -> StringSet.union (fv t1) (fv t2)
+
+
 
 (*
   ADD FUNCTIONS BELOW
